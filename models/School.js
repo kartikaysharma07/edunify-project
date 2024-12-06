7e99351aa55f74b782e16a3207dfbb2af60f583c
kartikaysharma07 <kartikay904@gmail.com>
2024-12-06 23:31:00 +0530

    new

--- models/School.js
+++ models/School.js
@@ -1,15 +1,41 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../src/app/db/sequelize');
+const { DataTypes } = require('sequelize'); // Import Sequelize DataTypes
+const sequelize = require('../db/sequelize'); // Import the Sequelize instance
 
+// Define the "School" model
 const School = sequelize.define('School', {
-    name: { type: DataTypes.TEXT, allowNull: false },
-    address: { type: DataTypes.TEXT, allowNull: false },
-    city: { type: DataTypes.TEXT, allowNull: false },
-    state: { type: DataTypes.TEXT, allowNull: false },
-    contact: { type: DataTypes.BIGINT, allowNull: false },
-    image: { type: DataTypes.TEXT, allowNull: false },
-    email_id: { type: DataTypes.TEXT, allowNull: false },
+    name: {
+        type: DataTypes.TEXT,
+        allowNull: false, // This field is required
+    },
+    address: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    city: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    state: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    contact: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+    },
+    image: {
+        type: DataTypes.TEXT,
+        allowNull: false, // URL or path to the image
+    },
+    email_id: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+            isEmail: true, // Ensures the value is a valid email
+        },
+    },
 });
 
+// Export the model
 module.exports = School;
 
